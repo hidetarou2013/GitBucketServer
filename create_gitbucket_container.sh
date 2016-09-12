becdd540cd9b377dcb7e1c79399bf9861b8ffa19
maekawa@Panasonic-PC <hidetoshi_maekawa@e-it.co.jp>
2016-09-12 17:45:33 +0900

    refs #1 initial commit

---
+++ create_gitbucket_container.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# run shell
+# sh create_gitbucket_container.sh gitbucket160912 60912 29418
+# sh create_gitbucket_container.sh gitbucket160913 60913 29913
+#
+# param1: container name
+# param2: gitbucket web service port
+# param3: gitbucket ssh service port
+
+$ mkdir -p /home/vagrant/gitbucket/data/$1
+$ docker run --name $1  -d -p $2:8080  -p $3:$3 -v /home/vagrant/gitbucket/data/$1:/gitbucket sken/gitbucket
+
+echo "access url : http://192.168.44.128:$2" 
+echo "root/root" 
+docker ps
+
+exit 0
