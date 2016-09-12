8517ebb34bb71fb9f2e2851af1869035e2320c5a
maekawa@Panasonic-PC <hidetoshi_maekawa@e-it.co.jp>
2016-09-12 17:52:31 +0900

    refs #1 modify shell

--- create_gitbucket_container.sh
+++ create_gitbucket_container.sh
@@ -8,8 +8,8 @@
 # param2: gitbucket web service port
 # param3: gitbucket ssh service port
 
-$ mkdir -p /home/vagrant/gitbucket/data/$1
-$ docker run --name $1  -d -p $2:8080  -p $3:$3 -v /home/vagrant/gitbucket/data/$1:/gitbucket sken/gitbucket
+mkdir -p /home/vagrant/gitbucket/data/$1
+docker run --name $1  -d -p $2:8080  -p $3:$3 -v /home/vagrant/gitbucket/data/$1:/gitbucket sken/gitbucket
 
 echo "access url : http://192.168.44.128:$2" 
 echo "root/root" 
